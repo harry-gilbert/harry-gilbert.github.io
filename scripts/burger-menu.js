c26816dc3997e8f927b9939e40c6fe06a5a1b61e
Harry Gilbert <48242947+Harry-Gilbert@users.noreply.github.com>
2021-02-28 12:10:00 +0000

    rebuilt kinetic typing system

--- scripts/burger-menu.js
+++ scripts/burger-menu.js
@@ -69,6 +69,22 @@ const toggleDM = () => {
     }
 }
 
+const typeInCont = () => {
+    let elems = document.getElementsByClassName("typingElem");
+    for (let i=0; i<elems.length; i++) {
+        let copy = elems[i].dataset.toType;
+        let count = 0;
+        let interval = 100;
+        setTimeout(function loop(){
+            elems[i].innerHTML += copy[count];
+            count++;
+            if (count >= copy.length) return;
+            setTimeout(loop, interval);
+        }, interval);
+    }
+}
+
+
 let currentMode = localStorage.getItem("currentMode");
 if ( currentMode == "lm" ) {
     toggleDM();
